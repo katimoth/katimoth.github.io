a7461e0da88402b82864f5bcdde7db6b2e7e8641
Tim Kao <katimoth@umich.edu>
2022-01-02 13:40:52 -0500

    remove bot images and sound

--- index.js
+++ index.js
@@ -77,12 +77,12 @@ function addChat(input, product) {
   let botImg = document.createElement("img");
   let botText = document.createElement("span");
   botDiv.id = "bot";
-  botImg.src = "bot-mini.png";
+  // botImg.src = "bot-mini.png";
   botImg.className = "avatar";
   botDiv.className = "bot response";
   botText.innerText = "Typing...";
   botDiv.appendChild(botText);
-  botDiv.appendChild(botImg);
+  // botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
   // Keep messages at most recent
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
