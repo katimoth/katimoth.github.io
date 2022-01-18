23b438b399dd0f2664dddfc43db4c75e85113451
Tim Kao <katimoth@umich.edu>
2022-01-18 17:40:14 -0500

    new routes

---
+++ index1.js
@@ -0,0 +1,93 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  // const inputField = document.getElementById("input");
+  // inputField.addEventListener("keydown", (e) => {
+  //   if (e.code === "Enter") {
+  //     let input = inputField.value;
+  //     inputField.value = "";
+  //     output(input);
+  //   }
+  // });
+
+  // var inputElement = document.getElementsByClassName('input');
+  // inputElement.type = "button"
+  // inputElement.addEventListener('click', function(){
+  //     console.log(inputElement.textContent);
+  //     output(inputElement.textContent);
+  // });
+  output();
+});
+
+// function handleClick(id) {
+//   const inputField = document.querySelector("#" + id);
+//   console.log(inputField.textContent);
+//   output(inputField.textContent);
+// }
+
+function output() {
+  let product = "It was fine, I was asked to look at numbers and remember them and on some of the rounds there was a beeping noise in the background."
+  addChat(product);
+  product = "They are probably trying to distract us with the noises to see how it affects our memory. I just tried to keep repeating the numbers in my head."
+  setTimeout(() => {
+    addChat(product);
+    product = "One of the noises was pretty annoying, but not painful."
+  }, 4000
+  )
+  setTimeout(() => {
+    addChat(product);
+  }, 8000
+  )
+}
+
+// function compare(promptsArray, repliesArray, string) {
+//   let reply;
+//   let replyFound = false;
+//   for (let x = 0; x < promptsArray.length; x++) {
+//     for (let y = 0; y < promptsArray[x].length; y++) {
+//       if (promptsArray[x][y] === string) {
+//         let replies = repliesArray[x];
+//         reply = replies[Math.floor(Math.random() * replies.length)];
+//         replyFound = true;
+//         // Stop inner loop when input value matches prompts
+//         break;
+//       }
+//     }
+//     if (replyFound) {
+//       // Stop outer loop when reply is found instead of interating through the entire array
+//       break;
+//     }
+//   }
+//   return reply;
+// }
+
+function addChat(product) {
+  const messagesContainer = document.getElementById("messages");
+
+  // let userDiv = document.createElement("div");
+  // userDiv.id = "user";
+  // userDiv.className = "user response";
+  // userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+  // messagesContainer.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  // let botImg = document.createElement("img");
+  let botText = document.createElement("span");
+  botDiv.id = "bot";
+  // botImg.src = "bot-mini.png";
+  // botImg.className = "avatar";
+  botDiv.className = "bot response";
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  // botDiv.appendChild(botImg);
+  messagesContainer.appendChild(botDiv);
+  // Keep messages at most recent
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+
+  // Fake delay to seem "real"
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+    // textToSpeech(product)
+  }, 4000
+  )
+
+}
---
+++ index2.js
@@ -0,0 +1,74 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  // const inputField = document.getElementById("input");
+  // inputField.addEventListener("keydown", (e) => {
+  //   if (e.code === "Enter") {
+  //     let input = inputField.value;
+  //     inputField.value = "";
+  //     output(input);
+  //   }
+  // });
+
+  // var inputElement = document.getElementsByClassName('input');
+  // inputElement.type = "button"
+  // inputElement.addEventListener('click', function(){
+  //     console.log(inputElement.textContent);
+  //     output(inputElement.textContent);
+  // });
+  output();
+});
+
+// function handleClick(id) {
+//   const inputField = document.querySelector("#" + id);
+//   console.log(inputField.textContent);
+//   output(inputField.textContent);
+// }
+
+function output() {
+  let product = "It was not great. I was asked to look at numbers and remember them and on some of the rounds there was a beeping noise in the background."
+  addChat(product);
+  product = "They are probably trying to distract us with the noises to see how it affects our memory. I just tried to keep repeating the numbers in my head."
+  setTimeout(() => {
+    addChat(product);
+    product = "I really didn't like the beeps because I have an auditory condition called tinnitus that make higher-pitched noises feel distressing and painful, but I think you would be okay if you don't have anything like that."
+  }, 4000
+  )
+
+  // Update DOM
+  setTimeout(() => {
+    addChat(product);
+  }, 8000
+  )
+}
+
+function addChat(product) {
+  const messagesContainer = document.getElementById("messages");
+
+  // let userDiv = document.createElement("div");
+  // userDiv.id = "user";
+  // userDiv.className = "user response";
+  // userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+  // messagesContainer.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  // let botImg = document.createElement("img");
+  let botText = document.createElement("span");
+  botDiv.id = "bot";
+  // botImg.src = "bot-mini.png";
+  // botImg.className = "avatar";
+  botDiv.className = "bot response";
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  // botDiv.appendChild(botImg);
+  messagesContainer.appendChild(botDiv);
+  // Keep messages at most recent
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+
+  // Fake delay to seem "real"
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+    // textToSpeech(product)
+  }, 4000
+  )
+
+}
